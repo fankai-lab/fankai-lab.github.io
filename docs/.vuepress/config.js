d7d9ffe109f61992b812295f9eabf2f18db947fa
fankai <18079017011@163.com>
2022-08-05 10:40:45 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,10 @@
 module.exports = {
+  title: '泡狗', //站点标题
+  description: '前端面试题库',//mate标签description，多用于搜索引擎抓取摘要
   // 其它配置
   themeConfig: {
+    siteTitle: "泡狗",
+    logo: "/javascript.jpg",
     searchMaxSuggestions: 10, // 对内置的搜索进行最多结果数量的限制
     nav: [
       { text: '首页', link: '/' },
